85c36d43eac97f357bb0c725f86596258c703cb8
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-10-29 12:04:55 +0900

    02_03確認課題

--- main.cpp
+++ main.cpp
@@ -1,36 +1,18 @@
+#include "Animal.h"
 #include <stdio.h>
-#include <iostream>
-#include <type_traits>
-
-// クラステンプレート
-template <typename T, typename U>
-class Minlator
-{
-
-public:
-    T min(T a, U b) {
-        return (a < b) ? a : b;
-    }
-
-};
+#include <vector>
+#include <memory>
 
 int main() {
+    
+    std::vector<std::unique_ptr<Animal>> animals;
+    
+    animals.push_back(std::make_unique<Dog>());
+    animals.push_back(std::make_unique<Cat>());
 
-
-    Minlator<int, int> intInt;
-    Minlator<int, float> intFloat;
-    Minlator<int, double> intDouble;
-    Minlator<float, float> floatFloat;
-    Minlator<float, double> floatDouble;
-    Minlator<double, double> doubleDouble;
-
-
-    printf("int, int: %d\n", intInt.min(3, 5));
-    printf("int, float: %.1f\n", intFloat.min(3, 5.5f));
-    printf("int, double: %.1f\n", intDouble.min(3, 5.5));
-    printf("float, float: %.1f\n", floatFloat.min(3.3f, 5.5f));
-    printf("float, double: %.1f\n", floatDouble.min(3.3f, 5.5));
-    printf("double, double: %.1f\n", doubleDouble.min(3.3, 5.5));
+    for (const auto& animal : animals) {
+        animal->makeSound();
+    }
 
     return 0;
 }
