3e7e322521b51f66b7a8bb12bffba8d4a3477e33
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2024-10-29 12:08:26 +0900

    g

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,9 +1,11 @@
 #include "Enemy.h"
-
+#include <cstdio>
 
 void (Enemy::* Enemy::functionTable[])() = {
+
     &Enemy::Getcloser,
     &Enemy::separation
+
 };
 
 Enemy::Enemy() : Phase(0) {}
@@ -14,19 +16,24 @@ void Enemy::Update() {
     (this->*functionTable[Phase])();
 
     // 行列の更新 (例: デバッグ表示)
-    std::cout << "Updating matrices..." << std::endl;
+    printf("Updating matrices...\n");
 
     // デバッグ用の表示
-    std::cout << "Current phase: " << Phase << std::endl;
+    printf("Current phase: %d\n", Phase);
 
     // フェーズ遷移 (次のフェーズに進む)
     Phase = (Phase + 1) % 2;
+
 }
 
 void Enemy::Getcloser() {
-    std::cout << "Enemy is approaching." << std::endl;
+
+    printf("Enemy is approaching.\n");
+
 }
 
 void Enemy::separation() {
-    std::cout << "Enemy is retreating." << std::endl;
+
+    printf("Enemy is retreating.\n");
+
 }
